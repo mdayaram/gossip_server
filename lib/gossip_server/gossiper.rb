80ca4a524325553b3b866296fad7428c739b6367
Manoj Dayaram <noj@squareup.com>
2018-02-12 22:05:20 -0800

    potentially working gossiper, not tested though

--- lib/gossip_server/gossiper.rb
+++ lib/gossip_server/gossiper.rb
@@ -4,10 +4,17 @@ module GossipServer
   class Gossiper
     attr_reader :my_id
     attr_reader :peers
+    attr_reader :world_state
+
+    attr_reader :messages_cache
+    attr_reader :messages_seen
 
     def initialize(id:, seed_id:)
       @my_id = id.to_s
       @peers = Set.new
+      @messages_seen = Set.new
+      @messages_cache = []
+      @world_state = {}
 
       peers << seed_id.to_s if seed_id
     end
@@ -20,8 +27,33 @@ module GossipServer
       peers.to_a
     end
 
-    def gossip_handler(msg = {})
-      msg
+    # Messages is an array, each element a hash of the following:
+    #   uuid:       string
+    #   client_id:  string
+    #   version:    int
+    #   ttl:        int
+    #   payload:    string
+    def gossip_handler(messages)
+      # Ignore messages we've seen or have had their TTL expire.
+      messages = messages.select { |m| m[:ttl] > 0 && !messages_seen.include?(m[:uuid]) }
+
+      # Update our world view
+      messages.each do |m|
+        update_world_state(
+          client_id: m[:client_id],
+          version: m[:version],
+          payload: m[:payload]
+        )
+      end
+
+      # Cache these messages
+      messages.each do |m|
+        m[:ttl] -= 1
+        messages_cache << m
+        messages_seen << m[:uuid]
+      end
+
+      "OK"
     end
 
     def fetch_peers!
@@ -37,10 +69,32 @@ module GossipServer
       end
     end
 
+    def gossip_peers!
+      peers_to_gossip = [peers.to_a.sample].compact # Only pick one for now.
+      return if peers_to_gossip.empty?
+
+      peers_to_gossip.each do |p|
+        res = HTTP.post("#{peer_host(p)}/gossip", json: message_cache).to_s
+
+        # If this peer did not like our gossip, remove it from our network
+        peers.delete(p) if res != "OK"
+      end
+    end
+
     private
 
     def peer_host(peer_id)
       "http://localhost:#{peer_id}"
+    end
+
+    def update_world_state(client_id:, version:, payload:)
+      if world_state[client_id].nil?
+        world_state[client_id] = {version: version, payload: payload}
+      elsif world_state[client_id][:version] < version
+        world_state[client_id] = {version: version, payload: payload}
+      else
+        world_state[client_id] # Keep current state, version was older.
+      end
     end
   end
 end
